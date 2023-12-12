1a52341ec592cdf79cd3fd3f4a44e67165e8ea90
AS005020 <evgenysabo@gmail.com>
2023-12-12 22:22:34 +0300

    spring initial

---
+++ backend/src/test/java/com/spring/web/api/backend/RestInPeaceMyBruddaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web.api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestInPeaceMyBruddaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
