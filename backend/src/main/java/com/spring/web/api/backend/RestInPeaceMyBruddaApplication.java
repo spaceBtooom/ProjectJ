a079bcbd3faf310954fa6922a4a912adaedc4a10
AS005020 <evgenysabo@gmail.com>
2023-12-14 20:09:23 +0300

    Added postgres' interaction with app
    MAIN PROBLEM THAT OCCURRED: Gradle(Reload all gradle projects)

--- backend/src/main/java/com/spring/web/api/backend/RestInPeaceMyBruddaApplication.java
+++ backend/src/main/java/com/spring/web/api/backend/RestInPeaceMyBruddaApplication.java
@@ -1,17 +1,43 @@
 package com.spring.web.api.backend;
 
+import com.spring.web.api.backend.domain.UserEntity;
+import com.spring.web.api.backend.repositories.UserEntityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestInPeaceMyBruddaApplication {
+public class RestInPeaceMyBruddaApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(UserEntityRepository.class);
+
+	private final UserEntityRepository userEntityRepository;
+
+	public RestInPeaceMyBruddaApplication(UserEntityRepository userEntityRepository) {
+		this.userEntityRepository = userEntityRepository;
+	}
+
+	public static void main(String[] args) {
         SpringApplication.run(RestInPeaceMyBruddaApplication.class, args);
     }
 
-    //Test
-    //Test2
-    //mytest
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Preloading " + userEntityRepository
+			.save(UserEntity
+			.builder()
+				.username("bil")
+				.name("billi")
+			.build()));
+
+		log.info("Preloading " + userEntityRepository
+			.save(UserEntity
+				.builder()
+				.username("bob")
+				.name("bobby")
+				.build()));
+	}
 }
