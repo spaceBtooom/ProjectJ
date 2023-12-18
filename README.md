899b925329a8a979d4f58d58ea2ebc4f53a2e5fa
AS005020 <evgenysabo@gmail.com>
2023-12-18 10:05:00 +0300

    Change hierarchy of frontend. Add test view.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ToDooshka
-И палка стреляет раз в год
+#docker installation
+Instruction:
+	1. docker-compose build
+	2. docker-compose up
+	3. localhost:8080 - api
+	4. postgres:5430 - (BD_NAME: API_BD, username:user, password:pass)
--- backend/src/main/java/com/spring/web/api/backend/repositories/UserEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.web.api.backend.repositories;
-
-import com.spring.web.api.backend.domain.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserEntityRepository extends CrudRepository<UserEntity, String> {
-}
